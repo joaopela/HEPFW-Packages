88e05ab95a0d35242338c9ef961ecc9645a620c3
Joao Pela <joaopela@gmail.com>
2013-12-03 23:13:47 +0000

    Initial definition for the header

---
+++ Latex/Tables/interface/ICTableCaption.h
@@ -0,0 +1,18 @@
+#ifndef LATEX_TABLES_ICTABLECAPTION
+#define LATEX_TABLES_ICTABLECAPTION
+
+// C++ Included
+#include <string>
+
+class ICTableCaption {
+public:
+
+  ICTableCaption();
+    
+private:
+
+  std::string m_caption;
+  
+};
+
+#endif
