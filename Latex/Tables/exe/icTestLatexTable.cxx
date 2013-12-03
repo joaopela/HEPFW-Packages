d20cbc4ff5d56c69a1ac0283cf27537c37aa1fbd
Joao Pela <joaopela@gmail.com>
2013-12-03 19:56:11 +0000

    Added example of creating and filling a tabular, and in turn adding that tabular to a table;

--- Latex/Tables/exe/icTestLatexTable.cxx
+++ Latex/Tables/exe/icTestLatexTable.cxx
@@ -2,6 +2,7 @@
 
 // ICTools includes
 #include "Latex/Tables/interface/ICTable.h"
+#include "Latex/Tables/interface/ICTabular.h"
 
 // Standard libraries includes
 #include <iostream>
@@ -11,11 +12,20 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-  ICTable table1;
+  ICTable table0;  
+  table0.setCentering(true);
+  table0.setLabel("tableTest01");
+
+  ICTabular tabular0(2,2);
   
-  table1.setCentering(true);
-  table1.setLabel("tableTest01");
-  table1.saveAs("text0.tex");
+  tabular0.set(0,0,"1");
+  tabular0.set(0,1,"2");
+  tabular0.set(1,0,"3");
+  tabular0.set(1,1,"3");
+  
+  table0.innerObjects.push_back(&tabular0);
+  
+  table0.saveAs("text0.tex");
   
   return 0;
 
