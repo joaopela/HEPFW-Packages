6864b73d6228e7337ad2e6307cab8b566320013d
Joao Pela <pela@cern.ch>
2013-12-09 19:10:01 +0100

    Initial and partial implementation of set methods for different variable types

--- Latex/Tables/src/ICTabularCell.cxx
+++ Latex/Tables/src/ICTabularCell.cxx
@@ -2,10 +2,49 @@
 
 using namespace std;
 
-void ICTabularCell::set(std::string value){m_cells = value;}
+ICTabularCell::ICTabularCell(){
+  
+  cellType = kString;
+
+  
+}
+  
+void ICTabularCell::set(std::string value){
+  cellType = kString;
+  m_cells  = value; 
+}
+
+void ICTabularCell::set(bool value){
+  cellType = kBool;
+  m_cells  = value;   
+}
+
+void ICTabularCell::set(int value){
+  cellType = kInt;
+  m_cells  = value; 
+}
+
+void ICTabularCell::set(unsigned value){
+  cellType = kUnsigned;
+  m_cells  = value; 
+}
+
+void ICTabularCell::set(float value){
+  cellType = kFloat;
+  m_cells  = value; 
+}
+
+void ICTabularCell::set(double value){
+  cellType = kDouble;
+  m_cells  = value; 
+}
 
 string ICTabularCell::toString(){
+ 
+  string out;
   
-  return m_cells;
+  if(cellType == kString){return m_cells;}
+  
+  
   
 }
