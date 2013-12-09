68cbac25e7d3474476480a37b8d1ac58b1766cfb
Joao Pela <joaopela@gmail.com>
2013-12-09 00:39:27 +0000

    Adding examples for new implemented features.

--- Latex/Tables/exe/icTestLatexTable.cxx
+++ Latex/Tables/exe/icTestLatexTable.cxx
@@ -14,15 +14,31 @@ int main(int argc, char *argv[]){
 
   ICTable table0;  
   table0.setCentering(true);
-  table0.setLabel("tableTest01");
+  table0.setPosition("htp");
+  table0.setLabel   ("tableTest01");
 
-  ICTabular tabular0(2,2);
+  ICTabular tabular0(3,3);
   
-  tabular0.set(0,0,"1");
-  tabular0.set(0,1,"2");
-  tabular0.set(1,0,"3");
-  tabular0.set(1,1,"3");
+  tabular0.setColumnDecorationBefore(0,"|");
+  tabular0.setColumnDecorationBefore(1,"||");
+  tabular0.setColumnDecorationBefore(2,"|");
+  tabular0.setColumnDecorationAfter (2,"|");
   
+  tabular0.setRowDecorationBefore(0,"\\hline");
+  tabular0.setRowDecorationBefore(1,"\\hline \\hline");
+  tabular0.setRowDecorationBefore(2,"\\hline");
+  tabular0.setRowDecorationAfter (2,"\\hline");
+
+  tabular0.set(0,0," ");
+  tabular0.set(0,1,"A");
+  tabular0.set(0,2,"B");
+  tabular0.set(1,0,"X");
+  tabular0.set(1,1,"1");
+  tabular0.set(1,2,"2");  
+  tabular0.set(2,0,"Y");
+  tabular0.set(2,1,"3");
+  tabular0.set(2,2,"4");  
+
   table0.innerObjects.push_back(&tabular0);
   
   table0.saveAs("text0.tex");
