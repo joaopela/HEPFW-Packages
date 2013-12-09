5f3d07ef2f55e75763da6f91ad289b3e1cc3737a
Joao Pela <pela@cern.ch>
2013-12-09 19:09:10 +0100

    Defining set methods for different types of possible variables. Adding enum to define each data type

--- Latex/Tables/interface/ICTabularCell.h
+++ Latex/Tables/interface/ICTabularCell.h
@@ -5,15 +5,34 @@
 #include <string>
 #include <vector>
 
+enum varType{
+  kBool,
+  kInt
+  kUnsigned,
+  kFloat
+  kDouble,
+  kString
+};
+
 class ICTabularCell {
 public:
   
+  ICTabularCell();
+  
   void set(std::string value);
+  void set(bool        value);
+  void set(int         value);
+  void set(unsigned    value);
+  void set(float       value);
+  void set(double      value);
+  
   std::string toString();
   
   
 private:
 
+  varType cellType;
+  
   std::string m_cells;
 
 };
