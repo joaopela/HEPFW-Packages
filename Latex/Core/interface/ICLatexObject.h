22dc4b30497ef4e7815d3845e585543b7be9cb5c
Joao Pela <pela@cern.ch>
2014-01-20 11:18:13 +0000

    Adding methods/variables to defined what packages are needed for this specific LaTeX object to compile. Removing default null toString() return.

--- Latex/Core/interface/ICLatexObject.h
+++ Latex/Core/interface/ICLatexObject.h
@@ -3,18 +3,27 @@
 
 // C++ Included
 #include <string>
+#include <set>
 
 class ICLatexObject {
 public:
   
   ICLatexObject(){};
 
-  virtual std::string toString(){return "";};
+  virtual std::string toString();
   void print();
+
   void saveAs(std::string fileName);
+  
+protected:
+  
+  void                  addRequiredPackage (std::string packageName);
+  std::set<std::string> getRequiredPackages();
+  std::string           getRequiredPackagesString();
   
 private:
 
+  std::set<std::string> m_requiredPackages;
 
 };
 
