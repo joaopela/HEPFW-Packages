a205d34a7d0afbfb37219d98ed0990c0d93b9f5a
Joao Pela <joaopela@gmail.com>
2014-06-16 15:14:48 +0100

    Adding example of setting initial value
    Added ilustration of how to iterate from an initial combination.

--- Algorithm/Mathmatics/example/ratExampleCombinationCalculator.cxx
+++ Algorithm/Mathmatics/example/ratExampleCombinationCalculator.cxx
@@ -11,17 +11,36 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-  rat::CombinationCalculator calc(8,3);
+  cout << "Now calculating all combinations of 3 out of 8 elements:" << endl;
+  rat::CombinationCalculator calc1(8,3);
   
   do {
-    vector<bool> combination = calc.getCombination();
+    vector<bool> combination = calc1.getCombination();
     
     for (unsigned i=0; i<combination.size(); i++) {
       if(combination[i]){cout << "1";}
       else              {cout << "0";}
     }
     cout << "\n";
-  } while (calc.nextCombination());
+  } while (calc1.nextCombination());
+  
+  //____________________________________________________________________________
+  cout << "Now calculating all combinations of 3 out of 5 elements but stating at combination (01010):" << endl;
+  
+  vector<bool> initialValue(5);
+  initialValue[1] = 1;
+  initialValue[3] = 1;
+  
+  rat::CombinationCalculator calc2(initialValue);  
+  do {
+    vector<bool> combination = calc2.getCombination();
+    
+    for (unsigned i=0; i<combination.size(); i++) {
+      if(combination[i]){cout << "1";}
+      else              {cout << "0";}
+    }
+    cout << "\n";
+  } while (calc2.nextCombination());  
   
   return 0;
 } 
