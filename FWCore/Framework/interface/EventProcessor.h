5a91ed96e63bbeee7b36ab84c85b854a28884bdd
Joao Pela <pela@cern.ch>
2014-11-14 16:10:23 +0000

    First working implementation that allows retrival of any know dataformat.

--- FWCore/Framework/interface/EventProcessor.h
+++ FWCore/Framework/interface/EventProcessor.h
@@ -2,7 +2,11 @@
 #define FWCore_Framework_EventProcessor
 
 // HEPFW includes
+#include "FWCore/Framework/interface/ConfigurationProcessor.h"
 #include "FWCore/Framework/interface/Dataset.h"
+#include "FWCore/Framework/interface/Event.h"
+// ROOT includes
+#include "TChain.h"
 
 // C++ STD includes
 #include <string>
@@ -18,12 +22,21 @@ namespace hepfw{
   public:
     
     EventProcessor();
+    EventProcessor(hepfw::ConfigurationProcessor* configProcessor);
     ~EventProcessor();
+    
+    Long64_t      getNEvent();
+    hepfw::Event* getEvent(Long64_t eventNumber);
     
   private:
     
-  };
+    
+    Long64_t                m_nEvent;
+    hepfw::Event           *m_event;
+    ConfigurationProcessor *m_configProcessor;
+    TChain                 *m_eventChain;
 
+  };
 }
 
 #endif  
--- FWCore/Framework/src/EventProcessor.cxx
+++ FWCore/Framework/src/EventProcessor.cxx
@@ -3,9 +3,41 @@
 using namespace std;
 
 hepfw::EventProcessor::EventProcessor(){
+  m_configProcessor = 0;
+  m_nEvent          = 0;
+//   m_event           = new hepfw::Event();
+}
+
+hepfw::EventProcessor::EventProcessor(hepfw::ConfigurationProcessor* configProcessor){
+
+  m_configProcessor = configProcessor;
+  vector<string> vInputFiles = m_configProcessor->getInputFiles();
+  
+  m_eventChain = new TChain("icEventProducer/EventTree");
+  
+  for(unsigned i=0; i<vInputFiles.size(); i++){
+    m_eventChain->Add(vInputFiles[i].c_str());
+  }
+
+  m_nEvent = m_eventChain->GetEntries();
 
 }
 
 hepfw::EventProcessor::~EventProcessor(){
 
+}
+
+Long64_t     hepfw::EventProcessor::getNEvent(){
+  return m_nEvent;
+}
+
+hepfw::Event* hepfw::EventProcessor::getEvent(Long64_t eventNumber){
+
+  hepfw::Event* ev = new hepfw::Event();
+  
+  ev->setTreeEventNumber(m_eventChain->LoadTree(eventNumber));
+  ev->setTree           (m_eventChain->GetTree());
+
+  return ev;
+    
 }
