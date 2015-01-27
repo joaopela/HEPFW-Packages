f7d4cb4d5f7f4467576109aa10e3c5fdc25ac57f
Joao Pela <pela@cern.ch>
2015-01-27 18:22:30 +0000

    Adding tau filter

--- AnalysisTools/Filters/interface/CollectionSizeFilterFwd.h
+++ AnalysisTools/Filters/interface/CollectionSizeFilterFwd.h
@@ -7,15 +7,18 @@
 // ICHiggsTauTau
 #include "DataFormats/ICHiggsTauTau/interface/Electron.h"
 #include "DataFormats/ICHiggsTauTau/interface/Muon.h"
+#include "DataFormats/ICHiggsTauTau/interface/Tau.h"
 
 namespace hepfw{
   
   typedef hepfw::CollectionSizeFilter<ic::Electron> ColSizeFilterElectron;
   typedef hepfw::CollectionSizeFilter<ic::Muon>     ColSizeFilterMuon;
-
+  typedef hepfw::CollectionSizeFilter<ic::Tau>      ColSizeFilterTau;
+  
 }
 
 #endif 
 
 //DECLARE_HEPFW_MODULE(hepfw::ColSizeFilterElectron);
 //DECLARE_HEPFW_MODULE(hepfw::ColSizeFilterMuon);
+//DECLARE_HEPFW_MODULE(hepfw::ColSizeFilterTau);
